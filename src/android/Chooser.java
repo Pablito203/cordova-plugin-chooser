35453f6592a85d6651748f2a6ab96adb7b769bc8
Pablo Martins <75085951+Pablito203@users.noreply.github.com>
2024-05-08 17:24:05 -0300

    remove base64 to prevent crashing when selecting large files on Android

--- src/android/Chooser.java
+++ src/android/Chooser.java
@@ -6,10 +6,10 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.MediaStore;
-import android.util.Base64;
 
-import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.Exception;
 
@@ -20,23 +20,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
 public class Chooser extends CordovaPlugin {
 	private static final String ACTION_OPEN = "getFile";
 	private static final int PICK_FILE_REQUEST = 1;
 	private static final String TAG = "Chooser";
-
-	/** @see https://stackoverflow.com/a/17861016/459881 */
-	public static byte[] getBytesFromInputStream (InputStream is) throws IOException {
-		ByteArrayOutputStream os = new ByteArrayOutputStream();
-		byte[] buffer = new byte[0xFFFF];
-
-		for (int len = is.read(buffer); len != -1; len = is.read(buffer)) {
-			os.write(buffer, 0, len);
-		}
-
-		return os.toByteArray();
-	}
 
 	/** @see https://stackoverflow.com/a/23270545/459881 */
 	public static String getDisplayName (ContentResolver contentResolver, Uri uri) {
@@ -56,11 +43,9 @@ public class Chooser extends CordovaPlugin {
 		return "File";
 	}
 
-
 	private CallbackContext callback;
-	private Boolean includeData;
 
-	public void chooseFile (CallbackContext callbackContext, String accept, Boolean includeData) {
+	public void chooseFile (CallbackContext callbackContext, String accept) {
 		Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
 		intent.setType("*/*");
 		if (!accept.equals("*/*")) {
@@ -68,8 +53,6 @@ public class Chooser extends CordovaPlugin {
 		}
 		intent.addCategory(Intent.CATEGORY_OPENABLE);
 		intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false);
-		intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
-		this.includeData = includeData;
 
 		Intent chooser = Intent.createChooser(intent, "Select File");
 		cordova.startActivityForResult(this, chooser, Chooser.PICK_FILE_REQUEST);
@@ -88,7 +71,7 @@ public class Chooser extends CordovaPlugin {
 	) {
 		try {
 			if (action.equals(Chooser.ACTION_OPEN)) {
-				this.chooseFile(callbackContext, args.getString(0), args.getBoolean(1));
+				this.chooseFile(callbackContext, args.getString(0));
 				return true;
 			}
 		}
@@ -107,33 +90,28 @@ public class Chooser extends CordovaPlugin {
 					Uri uri = data.getData();
 
 					if (uri != null) {
-						ContentResolver contentResolver =
-							this.cordova.getActivity().getContentResolver()
-						;
+						Activity activity = cordova.getActivity();
+						ContentResolver contentResolver = this.cordova.getActivity().getContentResolver();
+						InputStream inputStream = contentResolver.openInputStream(uri);
+						String uriString = uri.toString();
+						String filePath = null;
 
 						String name = Chooser.getDisplayName(contentResolver, uri);
+
+						filePath = activity.getCacheDir().getAbsolutePath() + '/' + name;
+            copyInputStreamToFile(inputStream, filePath);
 
 						String mediaType = contentResolver.getType(uri);
 						if (mediaType == null || mediaType.isEmpty()) {
 							mediaType = "application/octet-stream";
 						}
 
-						String base64 = "";
-
-						if (this.includeData) {
-							byte[] bytes = Chooser.getBytesFromInputStream(
-								contentResolver.openInputStream(uri)
-							);
-
-							base64 = Base64.encodeToString(bytes, Base64.DEFAULT);
-						}
-
 						JSONObject result = new JSONObject();
 
-						result.put("data", base64);
 						result.put("mediaType", mediaType);
 						result.put("name", name);
 						result.put("uri", uri.toString());
+						result.put("path", "file://" + filePath);
 
 						this.callback.success(result.toString());
 					}
@@ -153,4 +131,28 @@ public class Chooser extends CordovaPlugin {
 			this.callback.error("Failed to read file: " + err.toString());
 		}
 	}
+
+  private void copyInputStreamToFile(InputStream inputStream, String file) {
+        OutputStream out = null;
+
+        try {
+            out = new FileOutputStream(file);
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = inputStream.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
